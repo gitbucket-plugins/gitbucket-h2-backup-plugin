a16baa5faa82f45df762e3bd1fc8706d9fb232c7
Matthieu Brouillard <matthieu@brouillard.fr>
2016-05-10 08:53:57 +0200

    compatibility with gitbucket 4.0, fixes #10

--- project/build.scala
+++ project/build.scala
@@ -7,7 +7,7 @@ object MyBuild extends Build {
 
   val Organization = "fr.brouillard.gitbucket"
   val Name = "gitbucket-h2-backup-plugin"
-  val Version = "1.1"
+  val Version = "1.2.0"
   val ScalaVersion = "2.11.6"
 
   lazy val project = Project (
@@ -25,7 +25,8 @@ object MyBuild extends Build {
       "amateras-repo" at "http://amateras.sourceforge.jp/mvn/"
     ),
     libraryDependencies ++= Seq(
-      "gitbucket"          % "gitbucket-assembly" % "3.11.0" % "provided",
+      "gitbucket"          % "gitbucket-assembly" % "4.0.0" % "provided",
+      "io.github.gitbucket" % "solidbase" % "1.0.0" % "provided",
       "com.typesafe.play" %% "twirl-compiler"     % "1.0.4" % "provided",
       "javax.servlet"      % "javax.servlet-api"  % "3.1.0" % "provided"
     ),
--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,9 +1,8 @@
-import javax.servlet.ServletContext
-
 import fr.brouillard.gitbucket.h2.controller.H2BackupController
-import gitbucket.core.plugin.PluginRegistry
-import gitbucket.core.service.SystemSettingsService.SystemSettings
-import gitbucket.core.util.Version
+import io.github.gitbucket.solidbase.model.Version
+import gitbucket.core.controller.Context
+import gitbucket.core.plugin.Link
+
 
 class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "h2-backup"
@@ -13,25 +12,13 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val description: String = "Allows to export h2 database of gitbucket"
 
   override val versions: List[Version] = List(
-    Version(1, 0)
+    new Version("1.2.0"),
+    new Version("1.0.0")
   )
 
-  override def javaScripts(registry: PluginRegistry, context: ServletContext, settings: SystemSettings): Seq[(String, String)] = {
-    // Add Snippet link to the header
-    val path = settings.baseUrl.getOrElse(context.getContextPath)
-    Seq(
-      ".*/admin/h2backup" -> s"""
-        |$$('#system-admin-menu-container>li:last').after(
-        |  $$('<li class="active"><a href="${path}/admin/h2backup">H2 Backup</a></li>')
-        |);
-      """.stripMargin,
-      ".*/admin/(?!h2backup).*" -> s"""
-        |$$('#system-admin-menu-container>li:last').after(
-        |  $$('<li><a href="${path}/admin/h2backup">H2 Backup</a></li>')
-        |);
-      """.stripMargin
-    )
-  }
+  override val systemSettingMenus: Seq[(Context) => Option[Link]] = Seq(
+    (ctx: Context) => Some(Link("h2-backup", "H2 Backup", "admin/h2backup"))
+  )
 
   override val controllers = Seq(
     "/admin/h2backup" -> new H2BackupController()
