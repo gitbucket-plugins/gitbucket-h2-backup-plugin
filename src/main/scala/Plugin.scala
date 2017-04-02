cb0a070b3f74e0d14e02a99045eea7428cedb19d
adrian <a.adrian.tech@googlemail.com>
2017-04-02 04:11:59 +0200

    bump version number

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -15,7 +15,8 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     new Version("1.0.0"),
     new Version("1.2.0"),
     new Version("1.3.0"),
-    new Version("1.4.0")
+    new Version("1.4.0"),
+    new Version("1.5.0")
   )
 
   override val systemSettingMenus: Seq[(Context) => Option[Link]] = Seq(
